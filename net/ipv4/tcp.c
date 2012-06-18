69f162b1d77311d311654f484e2d7785fa5c11eb
hayashikejinan <hayashikejinan@gmail.com>
2012-06-18 21:53:22 +0900

    fix .23

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -3193,7 +3193,7 @@ void __init tcp_init(void)
 	}
 
 
-	cnt = tcp_hashinfo.ehash_size;
+	cnt = tcp_hashinfo.bhash_size;
 
 	tcp_death_row.sysctl_max_tw_buckets = cnt / 2;
 	sysctl_tcp_max_orphans = cnt / 2;
