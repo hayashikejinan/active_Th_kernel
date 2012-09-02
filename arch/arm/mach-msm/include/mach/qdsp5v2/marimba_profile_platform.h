3f7a98d61b12d780be68a91da4739d54c8268521
hayashikejinan <hayashikejinan@gmail.com>
2012-09-02 12:22:43 +0900

    update to 4.1.B.0.587
    Signed-off-by: hayashikejinan <hayashikejinan@gmail.com>

--- arch/arm/mach-msm/include/mach/qdsp5v2/marimba_profile_platform.h
+++ arch/arm/mach-msm/include/mach/qdsp5v2/marimba_profile_platform.h
@@ -47,10 +47,6 @@
 #include <mach/qdsp5v2/marimba_profile_satsuma.h>
 #elif defined(CONFIG_MACH_SEMC_MANGO)
 #include <mach/qdsp5v2/marimba_profile_mango.h>
-#elif defined(CONFIG_MACH_SEMC_AKANE)
-#include <mach/qdsp5v2/marimba_profile_akane.h>
-#elif defined(CONFIG_MACH_SEMC_AZUSA)
-#include <mach/qdsp5v2/marimba_profile_azusa.h>
 #elif defined(CONFIG_MACH_SEMC_AYAME)
 #include <mach/qdsp5v2/marimba_profile_ayame.h>
 #elif defined(CONFIG_MACH_SEMC_HAIDA)
