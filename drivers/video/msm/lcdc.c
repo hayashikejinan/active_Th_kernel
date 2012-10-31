22799c15e79a5c688652cdcc6513e76fc87906c3
hayashikejinan <hayashikejinan@gmail.com>
2012-11-01 00:39:35 +0900

    msm_fb: display: remove redundant turn off mdp footswitch.
    mdp footswitch is already turn off during mdp clk_disable in mdp_pipe_ctrl
    function hence removing the redundant call.
    
    Change-Id: Id0e6178210b836703618137e4c877f1e7036277a
    Signed-off-by: Rajesh Sastrula <vrajesh@codeaurora.org>

--- drivers/video/msm/lcdc.c
+++ drivers/video/msm/lcdc.c
@@ -52,7 +52,6 @@ static int pdev_list_cnt;
 
 static struct clk *pixel_mdp_clk; /* drives the lcdc block in mdp */
 static struct clk *pixel_lcdc_clk; /* drives the lcdc interface */
-struct regulator *mdp_footswitch;
 
 static struct platform_driver lcdc_driver = {
 	.probe = lcdc_probe,
@@ -72,9 +71,6 @@ static int lcdc_off(struct platform_device *pdev)
 	int ret = 0;
 
 	ret = panel_next_off(pdev);
-
-	if (mdp_footswitch)
-		regulator_disable(mdp_footswitch);
 
 	clk_disable(pixel_mdp_clk);
 	clk_disable(pixel_lcdc_clk);
@@ -134,9 +130,6 @@ static int lcdc_on(struct platform_device *pdev)
 
 	clk_enable(pixel_mdp_clk);
 	clk_enable(pixel_lcdc_clk);
-
-	if (mdp_footswitch)
-		regulator_enable(mdp_footswitch);
 
 	if (lcdc_pdata && lcdc_pdata->lcdc_power_save)
 		lcdc_pdata->lcdc_power_save(1);
@@ -289,10 +282,6 @@ static int __init lcdc_driver_init(void)
 
 	pm_qos_add_requirement(PM_QOS_SYSTEM_BUS_FREQ , "lcdc",
 			       PM_QOS_DEFAULT_VALUE);
-
-	mdp_footswitch = regulator_get(NULL, "fs_mdp");
-	if (IS_ERR(mdp_footswitch))
-		mdp_footswitch = NULL;
 
 	return lcdc_register_driver();
 }
