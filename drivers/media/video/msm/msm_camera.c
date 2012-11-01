854a22a122f02ce1771a3d532f3d6149f35eaf33
hayashikejinan <hayashikejinan@gmail.com>
2012-11-01 11:16:43 +0900

    media: camera: Remove redundant code
    Change-Id: Icd77cd5a558c3d88280af242447be719aa065e0c
    Signed-off-by: Nagesh Subba Reddy <nageshsreddy@codeaurora.org>

--- drivers/media/video/msm/msm_camera.c
+++ drivers/media/video/msm/msm_camera.c
@@ -2052,9 +2052,6 @@ static int msm_set_crop(struct msm_sync *sync, void __user *arg)
 			mutex_unlock(&sync->lock);
 			return -ENOMEM;
 		}
-	} else if (sync->croplen != crop.len) {
-		mutex_unlock(&sync->lock);
-		return -EINVAL;
 	}
 
 	if (copy_from_user(sync->cropinfo,
